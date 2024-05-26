4bece1edf4871946b93f8ec2dfebfd4b8748e229
Rudam Biswas <rudambiswas26@gmail.com>
2024-05-26 09:22:13 +0530

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,9 +2,9 @@ import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps
-   extends React.ButtonHTMLAttributes<HTMLButtonElements>{}
+   extends React.ButtonHTMLAttributes<HTMLButtonElement>{}
 
-const Button = forwardRef<HTMLButtonElements, ButtonProps>(({
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     className,
     children,
     disabled,
